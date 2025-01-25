4918f7d65f4f8c22a1256b25ed40a2f3217ba5e7
devbrunorm <devbrunorm@gmail.com>
2025-01-25 19:31:24 -0300

    feat: add task for kaggle export data

--- dags/main_dag.py
+++ dags/main_dag.py
@@ -1,12 +1,21 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+
+from modules.export import run_export
 
 with DAG(
     "main_dag",
-    start_date=datetime(2022, 1, 1),
+    start_date=datetime(2025, 1, 1),
     schedule_interval="@daily",
     catchup=False,
     tags=["olist", "dbt"]
 ):
-    dummy_task = DummyOperator(task_id = "test")
+    dummy_task = PythonOperator(
+        task_id = "export_data", 
+        python_callable = run_export, 
+            op_kwargs={
+                "kaggle_dataset": "olistbr/brazilian-ecommerce"
+            }
+    )
---
+++ dags/modules/export.py
@@ -0,0 +1,46 @@
+class Exporter:
+    def __init__(self, dataset: str):
+        self.dataset = dataset
+
+    def extract_from_kaggle(self) -> str:
+        """
+        This function downloads a dataset from Kaggle, returning the local for the downloded files.
+        """
+        import kagglehub
+
+        path = kagglehub.dataset_download(self.dataset)
+        return path
+
+
+    def list_all_downloaded_files(self, localpath: str) -> list:
+        """
+        This function lists all files in a given directory.
+        """
+        import os
+
+        return os.listdir(localpath)
+
+
+    def convert_csv_file_into_parquet(self, filepath: str) -> None:
+        """
+        This function converts a CSV file into a Parquet file.
+        """
+        import polars as pl
+        import os
+
+        print(filepath)
+        filename = filepath.split("/")[-1].split(".")[0]
+        output_path = f"/opt/airflow/datalake/bronze/{filename}.parquet"
+        pl.read_csv(filepath).write_parquet(output_path)
+        assert os.path.isfile(output_path)
+
+
+def run_export(kaggle_dataset: str) -> None:
+    """
+    This function orchestrates the export process.
+    """
+    exporter = Exporter(kaggle_dataset)
+    localpath = exporter.extract_from_kaggle()
+    files = exporter.list_all_downloaded_files(localpath)
+    for file in files:
+        exporter.convert_csv_file_into_parquet(f"{localpath}/{file}")
